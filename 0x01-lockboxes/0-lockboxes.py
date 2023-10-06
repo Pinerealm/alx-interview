643284e231e15c1795b21294f810d103c876d890
Pinerealm <osalami09@gmail.com>
2023-10-06 06:57:16 +0300

    Determine if all boxes can be opened

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+def canUnlockAll(boxes):
+    """Determines if all the boxes can be opened.
+
+    Args:
+        boxes (list): List of lists representing boxes and keys.
+
+    Returns:
+        bool: True if all boxes can be opened, False otherwise.
+    """
+    if not boxes or len(boxes) == 0:
+        return False
+
+    keys = [0]
+    for key in keys:
+        for box in boxes[key]:
+            # print(f'{keys} {box}')
+            if box not in keys and box < len(boxes):
+                keys.append(box)
+
+    if len(keys) == len(boxes):
+        return True
+    return False
