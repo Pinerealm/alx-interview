26826133f6f808f39a21072b9fbad4f1109de133
Pinerealm <osalami09@gmail.com>
2023-11-10 04:34:23 +0300

    Display characters of a specified Star Wars movie

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,35 @@
+#!/usr/bin/node
+// Prints all characters of a Star Wars movie
+
+const request = require('request');
+const movieId = process.argv[2];
+const url = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
+const charDict = {};
+
+request(url, (err, response, body) => {
+  if (err) {
+    console.log(err);
+  } else {
+    // Get the url of each character in the movie
+    const characters = JSON.parse(body).characters;
+    const charDict = {};
+
+    for (const character of characters) {
+      const charId = (character.split('/')[5]);
+      request(character, (err, response, body) => {
+        if (err) {
+          console.log(err);
+        } else {
+          charDict[charId] = JSON.parse(body).name;
+
+          // This preserves the order of the characters
+          if (Object.keys(charDict).length === characters.length) {
+            for (const id in charDict) {
+              console.log(charDict[id]);
+            }
+          }
+        }
+      });
+    }
+  }
+});
